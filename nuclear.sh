ade20b9b6fc9780195f6d581d52bf1078f41f1f7
Bruno do Rosario Petrucci <petrucci@iastate.edu>
2021-04-14 18:22:01 -0500

    Changed from strict to uncorrelated clocks so as to provide better mixing for the topology. Also started a nuclear and a morpho only run to compare.

---
+++ nuclear.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+
+#SBATCH --cpus-per-task=16
+
+#SBATCH --time=2-01:00:00
+
+#SBATCH --output=output_nuclear/job.%J.out
+
+#SBATCH --error=output_nuclear/job.%J.err
+
+#SBATCH --job-name="nuclear" 
+
+#SBATCH --mail-user=petrucci@iastate.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+module load cmake/3.18.1-j52h3yu
+module load gcc/7.3.0-xegsmw4
+module load boost/1.73.0-hm3w2fl
+
+cd /home/petrucci/canid_tree
+../revbayes/projects/cmake/rb scripts/nuclear_MCMC.Rev
